db65121f950d148688916579c1fbfa39b10221da
allyson_souza <souzaallyson53@gmail.com>
2025-10-22 15:08:33 -0300

    Em contrução tela Abuns especifico

--- Modelagem/desbrava.sql
+++ Modelagem/desbrava.sql
@@ -1,6 +1,15 @@
 CREATE DATABASE IF NOT EXISTS DESBRAVA;
 USE DESBRAVA;
 
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 22/10/2025 às 18:07
+-- Versão do servidor: 9.1.0
+-- Versão do PHP: 8.3.14
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -18,68 +27,76 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `albuns`
+-- Estrutura para tabela `albuns`
 --
 
 DROP TABLE IF EXISTS `albuns`;
 CREATE TABLE IF NOT EXISTS `albuns` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int NOT NULL AUTO_INCREMENT,
   `NOME` varchar(255) NOT NULL,
   `USUARIO` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID` (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Despejando dados para a tabela `albuns`
+--
+
+INSERT INTO `albuns` (`ID`, `NOME`, `USUARIO`) VALUES
+(8, 'Teste1', 'Teste1');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `alunos`
+-- Estrutura para tabela `alunos`
 --
 
 DROP TABLE IF EXISTS `alunos`;
 CREATE TABLE IF NOT EXISTS `alunos` (
-  `RE` int(11) NOT NULL,
-  `NOME` varchar(225) CHARACTER SET utf8 NOT NULL,
-  `USUARIO` varchar(225) CHARACTER SET utf8 NOT NULL,
-  `ESCOLA` varchar(225) CHARACTER SET utf8 NOT NULL,
+  `RE` int NOT NULL,
+  `NOME` varchar(225) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `USUARIO` varchar(225) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `ESCOLA` varchar(225) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `DATANASCIMENTO` date NOT NULL,
-  `GENERO` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
-  `TURMA` text CHARACTER SET utf8,
+  `GENERO` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `TURMA` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
   `PROFISSIONALRESPONSAVEL` char(11) NOT NULL,
-  `UF` char(2) CHARACTER SET utf8 NOT NULL,
-  `CIDADE` varchar(225) CHARACTER SET utf8 NOT NULL,
-  `DIAGNOSTICO` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `OBSERVACOES` varchar(1000) CHARACTER SET utf8 DEFAULT NULL,
-  `NIVELDELEITURA` int(1) DEFAULT NULL,
-  `NIVELDEESCRITA` int(1) DEFAULT NULL,
+  `UF` char(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `CIDADE` varchar(225) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `DIAGNOSTICO` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `OBSERVACOES` varchar(1000) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `NIVELDELEITURA` int DEFAULT NULL,
+  `NIVELDEESCRITA` int DEFAULT NULL,
   PRIMARY KEY (`RE`),
   UNIQUE KEY `USUARIO` (`USUARIO`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Extraindo dados da tabela `alunos`
+-- Despejando dados para a tabela `alunos`
 --
 
 INSERT INTO `alunos` (`RE`, `NOME`, `USUARIO`, `ESCOLA`, `DATANASCIMENTO`, `GENERO`, `TURMA`, `PROFISSIONALRESPONSAVEL`, `UF`, `CIDADE`, `DIAGNOSTICO`, `OBSERVACOES`, `NIVELDELEITURA`, `NIVELDEESCRITA`) VALUES
-(12345, 'João da Silva', 'joaosilva', 'Escola Municipal ABC', '2012-05-15', 'Masculino', '5A', '46502239897', 'SP', 'Campinas', 'Dislexia Leve', 'Observações fictícias sobre o aluno', 2, 1);
+(12345, 'João da Silva', 'joaosilva', 'Escola Municipal ABC', '2012-05-15', 'Masculino', '5A', '46502239897', 'SP', 'Campinas', 'Dislexia Leve', 'Observações fictícias sobre o aluno', 2, 1),
+(1, '1', '11', 'ESC ALTINA MAGALHAES DA SILVA', '1111-11-11', '1', '1', '49069495090', 'AC', 'Acrelândia', '1', '1', 2, 2);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `comentarios`
+-- Estrutura para tabela `comentarios`
 --
 
 DROP TABLE IF EXISTS `comentarios`;
 CREATE TABLE IF NOT EXISTS `comentarios` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int NOT NULL AUTO_INCREMENT,
   `USUARIO` varchar(255) NOT NULL,
-  `IDPOST` int(11) NOT NULL,
+  `IDPOST` int NOT NULL,
   `TEXTO` varchar(500) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Extraindo dados da tabela `comentarios`
+-- Despejando dados para a tabela `comentarios`
 --
 
 INSERT INTO `comentarios` (`ID`, `USUARIO`, `IDPOST`, `TEXTO`) VALUES
@@ -89,36 +106,66 @@ INSERT INTO `comentarios` (`ID`, `USUARIO`, `IDPOST`, `TEXTO`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `favoritos`
+-- Estrutura para tabela `dados_jogos`
 --
 
-DROP TABLE IF EXISTS `favoritos`;
-CREATE TABLE IF NOT EXISTS `favoritos` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ID_POST` int(11) NOT NULL,
-  `USUARIO` varchar(255) NOT NULL,
-  `ID_ALBUM` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID` (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `dados_jogos`;
+CREATE TABLE IF NOT EXISTS `dados_jogos` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `ID_ALUNO` varchar(11) NOT NULL,
+  `USUARIO_PROFISSIONAL` varchar(11) NOT NULL,
+  `NOME_JOGO` int NOT NULL,
+  `ID_NIVEL` int NOT NULL,
+  `PONTUACAO` int DEFAULT '0',
+  `PORCENTAGEM_COMPLETADA` decimal(5,2) DEFAULT '0.00',
+  `TEMPO_GASTO` time DEFAULT '00:00:00',
+  `ACERTOS` int DEFAULT '0',
+  `ERROS` int DEFAULT '0',
+  `TENTATIVAS` int DEFAULT '0',
+  `DATA_REGISTRO` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `post`
+-- Estrutura para tabela `favoritos`
+--
+
+DROP TABLE IF EXISTS `favoritos`;
+CREATE TABLE IF NOT EXISTS `favoritos` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `ID_POST` int NOT NULL,
+  `USUARIO` varchar(255) NOT NULL,
+  `ID_ALBUM` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID` (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+--
+-- Despejando dados para a tabela `favoritos`
+--
+
+INSERT INTO `favoritos` (`ID`, `ID_POST`, `USUARIO`, `ID_ALBUM`) VALUES
+(14, 16, 'Teste1', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `post`
 --
 
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `USUARIO` varchar(225) CHARACTER SET utf8 NOT NULL,
-  `LEGENDA` varchar(300) CHARACTER SET utf8 DEFAULT NULL,
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `USUARIO` varchar(225) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `LEGENDA` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `DATA_POST` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 --
--- Extraindo dados da tabela `post`
+-- Despejando dados para a tabela `post`
 --
 
 INSERT INTO `post` (`ID`, `USUARIO`, `LEGENDA`, `DATA_POST`) VALUES
@@ -148,7 +195,7 @@ INSERT INTO `post` (`ID`, `USUARIO`, `LEGENDA`, `DATA_POST`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `profissionais`
+-- Estrutura para tabela `profissionais`
 --
 
 DROP TABLE IF EXISTS `profissionais`;
@@ -170,7 +217,7 @@ CREATE TABLE IF NOT EXISTS `profissionais` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Extraindo dados da tabela `profissionais`
+-- Despejando dados para a tabela `profissionais`
 --
 
 INSERT INTO `profissionais` (`CPF`, `NOME`, `USUARIO`, `PROFISSAO`, `DATANASCIMENTO`, `UF`, `CIDADE`, `ESCOLA`, `SENHA`, `BIOGRAFIA`, `FOTOPERFIL`) VALUES
@@ -181,12 +228,12 @@ INSERT INTO `profissionais` (`CPF`, `NOME`, `USUARIO`, `PROFISSAO`, `DATANASCIME
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `profissoes`
+-- Estrutura para tabela `profissoes`
 --
 
 DROP TABLE IF EXISTS `profissoes`;
 CREATE TABLE IF NOT EXISTS `profissoes` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int NOT NULL AUTO_INCREMENT,
   `NOME` varchar(100) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID` (`ID`),
@@ -194,7 +241,7 @@ CREATE TABLE IF NOT EXISTS `profissoes` (
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
--- Extraindo dados da tabela `profissoes`
+-- Despejando dados para a tabela `profissoes`
 --
 
 INSERT INTO `profissoes` (`ID`, `NOME`) VALUES
